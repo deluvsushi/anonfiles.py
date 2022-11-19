a9e371e08bf51dbae69b585905d500b3a585b10d
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 19:34:28 +0800

    Update anonfiles.py

--- src/anonfiles.py
+++ src/anonfiles.py
@@ -3,20 +3,25 @@ from requests import post
 
 
 class Anonfiles:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://api.anonfiles.com"
 		self.headers = {
-			"User-Agent": "Mozilla/5.0 (Android; U; ru-RU) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/33.0"
+			"user-agent": "Mozilla/5.0 (Android; U; ru-RU) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/33.0"
 		}
 	
-	def upload_file(self, file: str):
-		data = {"file": (file, open(file, "rb"))}
+	def upload_file(self, file: str) -> dict:
+		data = {
+			"file": (
+				file,
+				open(file, "rb")
+			)
+		}
 		return post(
 			f"{self.api}/upload",
 			data=data,
 			headers=self.headers).json()
 		
-	def get_file_info(self, file_id: str):
+	def get_file_info(self, file_id: str) -> dict:
 		return get(
 			f"{self.api}/v2/file/{file_id}/info",
 			headers=self.headers).json()
